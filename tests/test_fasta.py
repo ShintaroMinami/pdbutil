f65c87f6f5e3598f28c8131fbc74dbaf3da83e39
Shintaro Minami <sminami@LocalMacBook.local>
2025-06-04 15:44:28 +0900

    updat fasta_io.py

--- tests/test_fasta.py
+++ tests/test_fasta.py
@@ -1,30 +1,17 @@
 import unittest
 from pathlib import Path
 try:
-    from pdbutil.fasta_io import FastaRecords, read_fasta
+    from pdbutil.fasta_io import FastaRecords, FastaRecord, read_fasta
 except ImportError:
     import sys
     sys.path.append(str(Path(__file__).resolve().parent.parent))
-    from pdbutil.fasta_io import FastaRecords, read_fasta
+    from pdbutil.fasta_io import FastaRecords, FastaRecord, read_fasta
 dir_path = Path(__file__).resolve().parent
 
 test_files = {
     "test_fasta": dir_path / "data/test.fasta"
     }
 
-'''
-class TestFastaSequence(unittest.TestCase):
-
-    def test_iter(self):
-        """Test the __iter__ method."""
-        fasta = FastaSequences(defline=["seq1"], sequence=["SEQUENCE"])
-        self.assertEqual(list(iter(fasta)), [["seq1"], ["SEQUENCE"]])
-
-    def test_len(self):
-        """Test the __len__ method."""
-        fasta = FastaSequences(defline=["seq1", "seq2"], sequence=["SEQUENCEONE", "SEQUENCETWO"])
-        self.assertEqual(len(fasta), 2)
-'''
 
 class TestReadFasta(unittest.TestCase):
 
@@ -36,6 +23,8 @@ class TestReadFasta(unittest.TestCase):
             f.write(">seq1\nSEQUENCEONE\n>seq2\nSEQUENCETWO\n")
 
         fasta = read_fasta(str(test_file))
+
+        print(fasta)
 
         self.assertEqual(len(fasta), 2)
         self.assertEqual(fasta.deflines, ["seq1", "seq2"], "Deflines do not match expected values")
