3313e65079184b251d14c051048d13d5bf5f1e46
Shintaro Minami <sminami@LocalMacBook.local>
2025-06-04 16:55:47 +0900

    update entry_point

--- pdbutil/main.py
+++ pdbutil/main.py
@@ -17,37 +17,38 @@ def pdb_rmsd():
     """
     from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
     parser = ArgumentParser(description='Calculate RMSD between two PDB files.', formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('pdb1', type=str, nargs='+', help='First group of PDB files.')
-    parser.add_argument('--pdb2', type=str, nargs='+', help='Second group of PDB files.')
+    parser.add_argument('-1', '--pdb1', type=str, nargs='+', help='First group of PDB files.')
+    parser.add_argument('-2', '--pdb2', type=str, nargs='+', help='Second group of PDB files.')
     parser.add_argument('--csv', action='store_true', help='Output in CSV format.')
     args = parser.parse_args()
 
     import numpy as np
     from pdbutil.pdb_io import read_pdb    
     from pdbutil.rmsd import calc_rmsd
+    from itertools import product
 
-    xyz1, file1 = [read_pdb(f)['xyz_ca'] for f in args.pdb1], args.pdb1
+    xyz1, file1 = [read_pdb(f, sidechain_warnings=False)['xyz_ca'] for f in args.pdb1], args.pdb1
 
     if not np.all(np.array([len(x) == len(xyz1[0]) for x in xyz1], dtype=bool)):
         raise ValueError("All input PDB files must have the same number of CA atoms.")
 
-    (xyz2, file2) = ([read_pdb(f)['xyz_ca'] for f in args.pdb2], args.pdb2) if args.pdb2 is not None else (xyz1, file1)
+    xyz2, file2 = ([read_pdb(f, sidechain_warnings=False)['xyz_ca'] for f in args.pdb2], args.pdb2) if args.pdb2 is not None else (xyz1, file1)
 
     if not np.all(np.array([len(x) == len(xyz1[0]) for x in xyz2], dtype=bool)):
         raise ValueError("All input PDB files must have the same number of CA atoms.")
 
     rmsd_mat = calc_rmsd(np.stack(xyz1, axis=0), np.stack(xyz2, axis=0))
-    file_mat = np.array(file1)[:,None] + ":" + np.array(file2)[None,:]
+    
     if args.csv:
         print("RMSD,File1,File2")
     else:
-        print(f" {'RMSD':>8}  {'File1':<20}  {'File2':<20}")
-    for rmsd, file_pair in zip(rmsd_mat.flatten(), file_mat.flatten()):
+        print(f" {'RMSD':>8}   File1,File2")
+    for rmsd, (f1, f2) in zip(rmsd_mat.flatten(), product(file1, file2)):
         if args.csv:
-            vals = [f"{rmsd:.5f}"] + file_pair.split(':')
+            vals = [f"{rmsd:.5f}"] + [f1, f2]
             print(",".join(vals))
         else:
-            print(f" {rmsd:8.5f}  {file_pair}")
+            print(f" {rmsd:8.5f}   {f1},{f2}")
 
 
 def pdb_superpose():
@@ -78,10 +79,11 @@ def pdb_superpose():
     import numpy as np
     from pdbutil.pdb_io import read_pdb, write_pdb
     from pdbutil.rmsd import superpose
+    from itertools import product
 
-    datadicts, names = [read_pdb(f) for f in args.pdbs], [str(Path(f).stem) for f in args.pdbs]
+    datadicts, names = [read_pdb(f, sidechain_warnings=False) for f in args.pdbs], [str(Path(f).stem) for f in args.pdbs]
 
-    xyz_ref = datadicts[0]['xyz_bb'] if args.reference is None else read_pdb(args.reference)['xyz_bb']
+    xyz_ref = datadicts[0]['xyz_bb'] if args.reference is None else read_pdb(args.reference, sidechain_warnings=False)['xyz_bb']
 
     if not np.all(np.array([len(d['xyz_bb']) == len(xyz_ref) for d in datadicts], dtype=bool)):
         raise ValueError("All input PDB files must have the same number of CA atoms.")
