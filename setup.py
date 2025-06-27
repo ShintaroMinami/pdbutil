a664bf21a3880fe32c002b5db7b6db8d5d866958
Shintaro Minami <sminami@LocalMacBook.local>
2025-06-27 12:12:38 +0900

    update ver

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(dir_path, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pdbutil',
-    version='2.1.1',
+    version='2.1.2',
     packages=['pdbutil'],
     license='MIT',
     url='https://github.com/ShintaroMinami/pdbutil',
